903ec257da692d9fea4b6ca632892597f9c4fb5c
msi-lap <antonioflores30@hotmail.com>
2023-03-22 09:46:53 -0600

    config to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import  FilePond from "./resources/assets/filepond/js/filepond.js";
+import   "./resources/assets/filepond/css/filepond.css";
+import   "./resources/assets/filepond/css/filepond-plugin-image-preview.css";
+import   "./resources/assets/filepond/js/filepond-plugin-image-preview.js";
 
 export default defineConfig({
     plugins: [
@@ -9,6 +13,18 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/assets/filepond/js/filepond.js',
                 'resources/assets/filepond/css/filepond.css',
+                'resources/assets/filepond/css/filepond-plugin-image-preview.css',
+                'resources/assets/filepond/js/filepond-plugin-image-preview.js',
+                
+ 
+                'public/theme/dist/assets/js/widgets.bundle.js',
+                'public/theme/dist/assets/plugins/custom/datatables/datatables.bundle.js',
+                'public/theme/dist/assets/css/style.bundle.css',
+                'public/theme/dist/assets/js/scripts.bundle.js',
+                'public/theme/dist/assets/css/style.bundle.rtl.css',
+                'public/theme/dist/assets/plugins/global/plugins.bundle.js'
+                
+
             ],
             refresh: true,
         }),
